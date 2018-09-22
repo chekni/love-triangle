67f392f181fb3f50c35700f0156fc9a2133266d1
Karina <karina_cheshko@mail.ru>
2018-09-23 00:58:06 +0300

    solve with mistake

--- src/index.js
+++ src/index.js
@@ -3,5 +3,38 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+
+    preferences.unshift(-1);
+
+  let index = [];
+
+  for (let i = 0; i <= preferences.length; i++){
+      index[i] = 0;
+  }
+    index[0] = 1;
+
+
+    for (let i = 1; i <= preferences.length && index[i] != 1; i++){
+        let first = index[i] == 0 ? preferences[i] : -1,
+         second = index[first] == 0 ? preferences[first] : -1,
+         third = index[second] == 0 ? preferences[second] : -1;
+
+        index[i] = 1;
+
+        if (first > 0 && second > 0 && third > 0){
+
+        if (first != second && first != third && second != third){
+            if (i == third){
+                index[first] = index[second] = 1;
+                count++;
+            }
+        }
+
+        }
+    }
+
+
+
+    return count;
 };
